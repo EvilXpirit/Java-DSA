30b84019a2e68df8ce694a6c6e59a5e1ec7e88e0
ADitya SHarma <adsharma122@gmail.com>
2023-10-16 22:47:46 +0530

    Stack

---
+++ StacksQueues/push/main.java
@@ -0,0 +1,22 @@
+package StacksQueues.push;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack(2);
+
+        System.out.println("Before push():");
+        System.out.println("--------------");
+        myStack.printAll();
+
+        myStack.push(1);
+
+        System.out.println("\n\nAfter push():");
+        System.out.println("-------------");
+        myStack.printAll();
+
+
+    }
+
+}
