8963677c846e9944574013800e0302edbe4deb54
ADitya SHarma <adsharma122@gmail.com>
2023-10-24 01:46:58 +0530

    hashmap3

--- Hash/hashmap/Hashmap.java
+++ Hash/hashmap/Hashmap.java
@@ -23,18 +23,18 @@ public class Hashmap {
             // (the buckets). In this case, it's initializing the array with four elements.
             // Each element will represent a bucket, and initially, **each bucket is an empty linked list.**
             for(int i = 0; i<4; i++){
-                this.buckets = new LinkedList<>();
+                this.buckets[i] = new LinkedList<>();
             }
         }
-        private int hashFunction(K key){
+        private int hashFunction(k key){
             int bi = key.hashCode();
-            return Maths.abs(bi) % N;
+            return Math.abs(bi) % N;
         }
 
-        private int searchInAll(k key, v value){
+        private int searchInAll(k key, int bi){
             LinkedList<Node> ll = buckets[bi]; //This line retrieves the linked list (bucket) from the buckets array based on the
             // provided bucket index bi. This linked list is assigned to the variable ll, so you can now search for the key within it.
-            for(int i=0; i<ll.size; i++){
+            for(int i=0; i<ll.size(); i++){
                 if(ll.get(i).key == key){
                     return i;
                 }
@@ -44,16 +44,16 @@ public class Hashmap {
 
 //        @SuppressWarnings("unchecked")
         private void rehash(){
-            Linkedlist<Node> oldBucket[] = buckets; //This line creates a reference oldBucket that points to the current array of buckets.
+            LinkedList<Node> oldBucket[] = buckets; //This line creates a reference oldBucket that points to the current array of buckets.
             // This is done so that you can access the old data while creating a new, larger array.
-            buckets = new Linkedlist[N*2]; //Here, you create a new array of buckets with double the number of buckets compared to the previous size (N * 2).
+            buckets = new LinkedList[N*2]; //Here, you create a new array of buckets with double the number of buckets compared to the previous size (N * 2).
             // The purpose of doubling the number of buckets is to reduce collisions and improve the efficiency of the hash map.
             for(int i=0; i<N; i++){
                 buckets[i] = new LinkedList<>(); //This loop initializes each bucket in the new buckets array as an empty linked list.
             }
             for(int i = 0; i< oldBucket.length; i++){
                 LinkedList<Node> ll = oldBucket[i];
-                for(int j = 0; j<ll.size()){
+                for(int j = 0; j<ll.size(); j++){
                     Node node = ll.get(j);
                     put(node.key, node.value);
                 }
---
+++ Hash/hashmap/main.java
@@ -0,0 +1,8 @@
+package Hash.hashmap;
+
+public class main {
+    public static void main(String[] args) {
+        //HashMap<String, Integer> map = new HashMap<>();
+        Hashmap.HashMap<String, Integer> map = new Hashmap.HashMap<>();
+    }
+}
